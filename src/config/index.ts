db26826be67a07817dcf7c03d980afc0b0997cc8
Meddy <meddyakpan@yahoo.co.uk>
2020-12-13 10:23:57 +0100

    Some small clean up

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,8 @@ import path from 'path';
 
 dotenv.config( { path: path.join(__dirname, '.env') });
 
-export { default as serverConfig } from './serverConfig';
+export { default as serverConfig } from './server.config';
 export { default as logger } from './logger';
-export { default as dbConfig } from './dbConfig';
-export { default as jwtConfig } from './jwtConfig';
+export { default as dbConfig } from './db.config';
+export { default as jwtConfig } from './jwt.config';
+export { default as swaggerConfig } from './swagger.config';
--- src/server.ts
+++ src/server.ts
@@ -6,17 +6,15 @@ import { serverConfig, logger, dbConfig } from './config';
 import identityRouter from './modules/identity/routes/identity.routes';
 
 const server = express();
-const { port } = serverConfig;
-const { db_host, db_name, db_user, db_password } = dbConfig;
 
 server.use(helmet());
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
-logger.info('Attempting to connect');
-
-
 (async () => {
+    const { port } = serverConfig;
+    const { db_host, db_name, db_user, db_password } = dbConfig;
+    logger.info('Attempting to connect');
     try {
         const connectionString = `mongodb://${db_user}:${db_password}@${db_host}/${db_name}`;
         await mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
